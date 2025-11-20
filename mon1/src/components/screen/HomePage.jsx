70fa2fd54cf84dd3c2f558ce73d1cde53733d2c9
mon <mon.dev005@gmail.com>
2025-11-20 15:07:51 +0800

    checkout page.

--- mon1/src/components/screen/HomePage.jsx
+++ mon1/src/components/screen/HomePage.jsx
@@ -4,6 +4,8 @@ import "../../assets/styles/header.css";
 function HomePage() {
   return (
     <>
+      <title>Ecom</title>
+
       <div className="header">
         <div className="left-section">
           <a href="index.html" className="header-link">
