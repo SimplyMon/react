406f05d08c40a620201b4133e1f6f3780a5b7925
mon <mon.dev005@gmail.com>
2025-11-20 15:19:39 +0800

    add orders page.

--- mon1/src/App.jsx
+++ mon1/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router";
-
 import HomePage from "./components/screen/HomePage";
 import CheckoutPage from "./components/screen/CheckoutPage";
+import OrderPage from "./components/screen/OrderPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrderPage />} />
     </Routes>
   );
 }
