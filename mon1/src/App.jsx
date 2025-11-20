652ddc2499e2fb1ec855a34c95167907bbc4a5d7
mon <mon.dev005@gmail.com>
2025-11-20 14:54:36 +0800

    create multiple page with react router.

--- mon1/src/App.jsx
+++ mon1/src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router";
+
 import HomePage from "./components/screen/HomePage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/checkout" element={<div>Hello</div>} />
+    </Routes>
+  );
 }
 
 export default App;
