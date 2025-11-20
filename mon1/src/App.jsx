ebd1fd26411f3ed0b85b38055d84b62cec4caa80
mon <mon.dev005@gmail.com>
2025-11-20 15:45:03 +0800

    generate data for products

--- mon1/src/App.jsx
+++ mon1/src/App.jsx
@@ -3,13 +3,18 @@ import HomePage from "./components/screen/HomePage";
 import CheckoutPage from "./components/screen/CheckoutPage";
 import OrderPage from "./components/screen/OrderPage";
 
+import HeaderComponent from "./components/layout/HeaderComponent";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/orders" element={<OrderPage />} />
-    </Routes>
+    <>
+      <HeaderComponent />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/orders" element={<OrderPage />} />
+      </Routes>
+    </>
   );
 }
 
